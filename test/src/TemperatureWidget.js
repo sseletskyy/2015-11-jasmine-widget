fde88f9bdbd117be531b473fb5bf4c949c974721
sseletskyy <sseletskyy@gmail.com>
2015-11-13 23:45:12 +0200

    clear out src file

--- test/src/TemperatureWidget.js
+++ test/src/TemperatureWidget.js
@@ -1,32 +1,3 @@
-function TemperatureWidget($parent) {
-    var self = this;
-    this.$parent = $parent;
-
-    this.getData = function(){
-        self.dataPromise = $.get('/api/temperature');
-    }
-    this.render = function () {
-
-        this.dataPromise.done(function (data) {
-            $widgetDiv = '<div><div class="temperature-widget">' +
-            data.temperature +
-            ' C</div>' +
-            '<button class="temperature-refresh"></button></div>';
-
-            $widgetDiv = $($widgetDiv).on("click", "button", function () {
-                $.proxy(self.update(), self);
-            });
-
-            self.$parent.html($widgetDiv);
-
-        });
-    };
-
-    this.update = function(){
-        self.getData();
-        self.render();
-    }
-
-    this.update();
+function TemperatureWidget() {
 }
 
